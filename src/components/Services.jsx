9dc7c056a02119919ac9a317c5530d02ac90de21
singhKrati <prakharkrati@gmail.com>
2023-10-08 16:01:49 +0530

    added services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import architectural_management from '../assets/architectural_management.jpeg'
+import construction_planning from '../assets/construction_planning.jpg'
+import construction_management from '../assets/construction_management.jpg'
+
+const Services = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Services
