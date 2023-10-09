5ae498aca47513dca4817a411c610254ff77c579
singhKrati <prakharkrati@gmail.com>
2023-10-09 20:19:35 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Home'
-import About from './components/About'
-import Projects from './components/Projects'
 import Completed from './pages/completed'
 import Ongoing from './pages/ongoing'
 import Upcoming from './pages/upcoming'
